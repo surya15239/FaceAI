f88151e81da6e2d82277d931c060677d051278f9
surya15239 <36006517+surya15239@users.noreply.github.com>
2021-12-23 15:55:49 +0530

    Add files via upload

---
+++ surya1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+from fer import FER
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image, ImageOps
+import imageio
+st.write('''
+#  Emotion Detector
+''')
+st.write("A Image Classification Web App That Detects the Emotions Based On An Image")
+file = st.file_uploader("Please Upload an image of Person With Face", type=['jpg','png'])
+if file is None:
+  st.text("Please upload an image file")
+else:
+  image = imageio.imread(file)
+  detector = FER(mtcnn=True)
+  result = detector.detect_emotions(image)
+  st.write(result)
+  st.image(image, use_column_width=True)
+
+
+# In[ ]:
+
+
+
+

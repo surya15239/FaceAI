0566a573ec0c720f34eb192eaf6aa2449fbc3c09
surya15239 <36006517+surya15239@users.noreply.github.com>
2021-12-24 11:33:01 +0530

    Update surya1.py

--- surya1.py
+++ surya1.py
@@ -20,7 +20,8 @@ else:
   emo_detector = FER(mtcnn=True)
   captured_emotions = emo_detector.detect_emotions(image)
 # Print all captured emotions with the image
-  st.write(sorted(captured_emotions)[1])#plt.imshow(image)
+  st.write(sorted(captured_emotions)[1])
+  #plt.imshow(image)
 
   # Use the top Emotion() function to call for the dominant emotion in the image
   dominant_emotion, emotion_score = emo_detector.top_emotion(image)
